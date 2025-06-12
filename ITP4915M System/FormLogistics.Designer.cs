a0d5434d010fb8db31ace6f9dfbdc1568313c04c
cheongkitleung <cheongkitleung@CHEONGKITLE5FEC>
2025-06-13 01:45:08 +0800

    13/06/2025

--- ITP4915M System/FormLogistics.Designer.cs
+++ ITP4915M System/FormLogistics.Designer.cs
@@ -1,8 +1,6 @@
-﻿using ITP4915MSystem;
-
-namespace ITP4915M_System
+﻿namespace ITP4915M_System
 {
-    partial class FormLogistics : FormTemplate
+    partial class FormLogistics
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +28,24 @@ namespace ITP4915M_System
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(590, 91);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // FormLogistics
             // 
-            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Name = "FormLogistics";
             Text = "FormLogistics";
             Load += FormLogistics_Load;
@@ -44,5 +53,7 @@ namespace ITP4915M_System
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- ITP4915M System/FormLogistics.cs
+++ ITP4915M System/FormLogistics.cs
@@ -7,11 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using ITP4915MSystem;
 
 namespace ITP4915M_System
 {
-    public partial class FormLogistics : FormTemplate
+    public partial class FormLogistics : Form
     {
         public FormLogistics()
         {
